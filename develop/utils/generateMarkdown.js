f629123eb98efcaa4055ca1991d95d0fa9455cce
Stephen Keenan <stephenkeenan02@gmail.com>
2020-11-14 17:12:47 -0500

    generateMarkdown update

--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -2,7 +2,35 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Description
+  ${data.Description}
+
+  ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [Contributors](#Contribution)
+  * [Testing](#Testing)
+  * [Licenses](#Licenses)
+  * [Questions](#)
+
+  ## Installation
+  ${data.Installation}
+
+  ## Usage
+  ${data.Usage}
+
+  ## Contributors
+  ${data.Contribution}
+
+  ## Testing
+  ${data.Testing}
+
+  ## Licenses
+  ${data.Licenses}
+
+  ## Questions
+  * <https://github.com/${data.Github}>
+  * <${data.Email}>`;
 }
 
 module.exports = generateMarkdown;
