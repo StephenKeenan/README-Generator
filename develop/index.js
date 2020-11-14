b14aa871f395e13921ebf2a557c3c1131b6534ed
Stephen Keenan <stephenkeenan02@gmail.com>
2020-11-14 15:48:40 -0500

    Question array

--- develop/index.js
+++ develop/index.js
@@ -1,4 +1,66 @@
 // array of questions for user
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUser = () =>
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'Table of Contents',
+            message: 'Table of Contents?',
+            choices: ['Description', 'Installation', 'Usage', 'Contribution', 'Testing', 'License', 'Questions'],
+        },
+        {
+            type: 'input',
+            name: 'Title',
+            message: 'Title?',
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Description?',
+        },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'Installation Instructions?',
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message: 'Usage Information?',
+        },
+        {
+            type: 'input',
+            name: 'Contribution',
+            message: 'Constributors?',
+        },
+        {
+            type: 'input',
+            name: 'Testing',
+            message: 'Test Instructions?',
+        },
+        {
+            type: 'list',
+            name: 'Licenses',
+            message: 'Licenses?',
+            choices: ['A', 'B', 'C', 'D'],
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username',
+        },
+        {
+            type: 'input',
+            name: 'Email',
+            message: 'Enter your contact email.',
+        },
+    ]);
+
 const questions = [
 
 ];
