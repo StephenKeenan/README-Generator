0a78b1c2e092f165f1e8dc1f61293c7eabd09558
Stephen Keenan <stephenkeenan02@gmail.com>
2020-11-14 22:51:08 -0500

    Index edit - redo

--- develop/index.js
+++ develop/index.js
@@ -1,4 +1,3 @@
-
 const inquirer = require('inquirer');
 const fs = require('fs');
 const build = require('./utils/generateMarkdown.js');
@@ -6,69 +5,60 @@ const build = require('./utils/generateMarkdown.js');
 
 // array of questions for user
 const questions = [
-        {
-            type: 'input',
-            name: 'Title',
-            message: 'Title?',
-        },
-        {
-            type: 'input',
-            name: 'Description',
-            message: 'Description?',
-        },
-        {
-            type: 'input',
-            name: 'Installation',
-            message: 'Installation Instructions?',
-        },
-        {
-            type: 'input',
-            name: 'Usage',
-            message: 'Usage Information?',
-        },
-        {
-            type: 'input',
-            name: 'Contribution',
-            message: 'Constributors?',
-        },
-        {
-            type: 'input',
-            name: 'Testing',
-            message: 'Test Instructions?',
-        },
-        {
-            type: 'list',
-            name: 'Licenses',
-            message: 'Licenses?',
-            choices: ['A', 'B', 'C', 'D'],
-        },
-        {
-            type: 'input',
-            name: 'Github',
-            message: 'Enter your GitHub Username',
-        },
-        {
-            type: 'input',
-            name: 'Email',
-            message: 'Enter your contact email.',
-        },
-    ],
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'Title?',
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Description?',
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'Installation Instructions?',
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'Usage Information?',
+    },
+    {
+        type: 'input',
+        name: 'Contribution',
+        message: 'Constributors?',
+    },
+    {
+        type: 'input',
+        name: 'Testing',
+        message: 'Test Instructions?',
+    },
+    {
+        type: 'list',
+        name: 'Licenses',
+        message: 'Licenses?',
+        choices: ['A', 'B', 'C', 'D'],
+    },
+    {
+        type: 'input',
+        name: 'Github',
+        message: 'Enter your GitHub Username',
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'Enter your contact email.',
+    },
+],
 
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) { }
 
 // function to initialize program
-function init() {
-
-    promptUser()
-
-        .then(function (data) {
-            const README = generateMarkdown(data);
-
-        })
-}
+function init() { }
 
 // function call to initialize program
 init();
