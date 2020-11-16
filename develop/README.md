4d04ca484dea149681c18a4b7a5f8319d973ea8c
Stephen Keenan <stephenkeenan02@gmail.com>
2020-11-16 14:14:15 -0500

    Work with tutor - review

---
+++ develop/README.md
@@ -0,0 +1,31 @@
+# 
+
+  ## Description
+  test
+
+  ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [Contributors](#Contribution)
+  * [Testing](#Testing)
+  * [Licenses](#Licenses)
+  * [Questions](#)
+
+  ## Installation
+  dfsdfg
+
+  ## Usage
+  fewf
+
+  ## Contributors
+  sfg
+
+  ## Testing
+  dffs
+
+  ## Licenses
+  B
+
+  ## Questions
+  * <https://github.com/gdfg>
+  * <dfk>
--- develop/index.js
+++ develop/index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const path = require('path')
 const fs = require('fs');
 const build = require('./utils/generateMarkdown.js');
 
@@ -8,12 +9,12 @@ const questions = [
     {
         type: 'input',
         name: 'Title',
-        message: 'Title?',
+        message: 'Title of Project?',
     },
     {
         type: 'input',
         name: 'Description',
-        message: 'Description?',
+        message: 'Description of Project?',
     },
     {
         type: 'input',
@@ -51,14 +52,33 @@ const questions = [
         name: 'Email',
         message: 'Enter your contact email.',
     },
-],
+]
+
 
 
 // function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
 
 // function to initialize program
-function init() { }
+function init() {
+inquirer
+  .prompt(questions)
+  .then(answers => {
+      console.log('test')
+    writeToFile("README.md", build({...answers}))
+    console.log('readme gen')
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+      console.log(error)
+    }
+  });
+ }
 
 // function call to initialize program
 init();
